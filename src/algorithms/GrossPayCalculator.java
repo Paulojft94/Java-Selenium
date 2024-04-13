0d5b2f6b737e13d4aad48ed898bf18fdcbbaa3cc
Paulojft94 <1222013@isep.ipp.pt>
2024-04-13 11:01:30 +0100

    add simple GrossPayCalculator algorithm

---
+++ src/algorithms/GrossPayCalculator.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+
+    public static void main(String arg [] ){
+
+        System.out.println("Enter the number of hours the employee worked:");
+
+        Scanner scanner = new Scanner(System.in);
+        int numHours = scanner.nextInt();
+
+        System.out.println("Enter the employee pay rate:");
+        double payRate = scanner.nextDouble();
+
+        double salary = numHours*payRate;
+
+        System.out.println("The employee's salary is: " + salary);
+    }
+
+}
